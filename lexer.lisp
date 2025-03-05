023f04783db100d5e9c5a05379afda84f1c4a3db
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-03-04 22:47:25 -0600

    global variables, recursion, new logic

--- lexer.lisp
+++ lexer.lisp
@@ -2,69 +2,67 @@
 ;input = String
 ;output = List of tokens
 
-(setq token nil)
-(setf listaPreTokens nil)
-(setf listaTokens nil)
+(defvar token nil)
+(defvar listaPreTokens nil)
+(defvar listaTokens nil)
+;defvar to declare global variables
 
 (defun concatenar (list)
-  ;Concatenates the elements of a list into a string.
-  (with-output-to-string (stream)
-  ;creates a character output stream, 
-  ;performs a series of operations that may send results to this stream, 
-  ;and then closes the stream.
+  "Concatenates the elements of a list into a string."
+  (with-output-to-string (stream);with-output-to-string is a macro that captures the output of the code inside it
     (dolist (element list)
-    ;dolist iterates over the elements of a list. 
-    ;The body of dolist consists of a series of tags and statements.
+    ;dolist is a macro that iterates over a list
       (format stream "~a" element))))
-      ;format writes the elements to the stream
 
 (defun tokenizerNoStructure (input)
-  ;Tokenizes the input string without considering the structure
-  ;rango de caracteres
+  "Tokenizes the input string without considering the structure."
   (loop for i from 0 below (length input)
-        ;es un espacio?
         do (if (not (char= (char input i) #\Space))
-                ;not returns content if value false, otherwise return nil
-                    ;char= compares characters
-                                    ;char gets element of string by index.
-            (progn ;progn executes multiple expressions in the then
-                      (push (char input i) token)
-                      ;push adds an element to the beginning of a list
-                      (loop for j from (1+ i) below (length input)
-                            do (if (not (char= (char input j) #\Space))
-                                   (push (char input j) token)
-                                   (return)))
-                      (setf token (nreverse token))
-                      ;nreverse returns the reverse of a list
-            )
-            (progn
-                  (push (concatenar (nreverse token)) listaPreTokens)
-                  (setf token nil)
-            )
-          )
-  )
-)
+        ;not is a function that returns true if the argument is false
+        ;char= compares characters
+               (progn;to use more than one expression in a loop
+                 (push (char input i) token)
+                 ;push adds an element to a list in the first position
+                 (loop for j from (1+ i) below (length input)
+                       do (if (char= (char input j) #\Space)
+                              (tokenizerNoStructure (subseq input (1+ j) (length input)))
+                              (push (char input j) token))
+                              )
+                 (setf token (nreverse token))
+                 ;nreverse returns the reverse of a list
+                 ;necesario ya que push agrega al principio
+                 (push (concatenar token) listaPreTokens)
+                 (setf token nil))
+               (progn
+                 (when token
+                 ;when macro that executes code if condition true
+                   (push (concatenar (nreverse token)) listaPreTokens))
+                 (setf token nil)))))
+
 
 (defun tokenizerWithStructure (listaPreTokens)
+  "Tokenizes the input list considering the structure."
   (loop for i from 0 below (length listaPreTokens)
-    do (if (char= (char (elt listaPreTokens i) 0) #\()
-            (loop while (not (char= (char (elt listaPreTokens i) 0) #\)))
-              do (progn
-                (push (elt listaPreTokens i) token)
-                (incf i) ;incrementa i
-                 ))
-            (progn
-              (push (concatenar (nreverse token)) listaTokens)
-              (setf token nil)
-            )
-        )
-  )      
+        do (if (char= (char (elt listaPreTokens i) 0) #\()
+        ;elt returns the element of a list at a given index
+               (loop while (and (< i (length listaPreTokens))
+                                (not (char= (char (elt listaPreTokens i) 0) #\))))
+                     do (progn
+                          (push (elt listaPreTokens i) token)
+                          (incf i))
+                          ;incf increments a variable
+               (progn
+                 (push (concatenar (nreverse token)) listaTokens)
+                 (setf token nil)))
+        (when token
+          (push (concatenar (nreverse token)) listaTokens)
+          (setf token nil))))
 )
 
-;test
+; Test
 (format t "Ingrese la expresion dejando un espacio entre atoms~%")
-(let ((a (read)))
-  (tokenizerNoStructure (princ-to-string a))
-  (tokenizerWithStructure (listaPreTokens))
-  (format t "Tokens: ~a~%" listaTokens)
-)
+;format is a function that writes to the output stream
+(let ((a (read-line)))
+  (tokenizerNoStructure a)
+  (tokenizerWithStructure listaPreTokens)
+  (format t "Tokens: ~a~%" listaTokens))
